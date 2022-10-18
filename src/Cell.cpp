35d0db1acc977ff656959479c8af5f1824a338db
Atqa Munzir <atqamz@gmail.com>
2022-10-18 20:31:48 +0700

    first commit

---
+++ src/Cell.cpp
@@ -0,0 +1,11 @@
+#include "header/Cell.h"
+
+Cell::Cell()
+{
+    this->value = 'A';
+};
+
+Cell::Cell(char _value)
+{
+    this->value = _value;
+};
---
+++ src/header/Cell.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Cell
+{
+public:
+    Cell();
+    Cell(char value);
+
+private:
+    char value;
+};
